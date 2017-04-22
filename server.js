22b4189673e223af86559bea91ac41eebe30cca0
Andrew Martin <surfgod@MacBook.local>
2017-04-22 14:38:22 +1200

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(`${__dirname}/partials`)
@@ -11,8 +13,7 @@ app.use((req, res, next) => {
     var now = new Date().toString();
 
     var log = (`${now}: ${req.method} ${req.url}`);
-    console.log(log);
-    fs.appendFile('server.log', `${log}\n`)
+    fs.appendFile('server.log', `${log}\n`);
     next();
 });
 
@@ -48,6 +49,6 @@ app.get('/bad', (req, res) => {
         errorMsg: 'Mother Fucker crashed my ass...'
     });
 });
-app.listen(3000, () => {
-    console.log(`Server is up on port 3000 ${__filename}`);
+app.listen(port, () => {
+    console.log(`Server is up on port ${port} ${__filename}`);
 });
